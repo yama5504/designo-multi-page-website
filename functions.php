7d73565d912207480965c70f6e2399696bac2906
yama <yamagaku5504@gmail.com>
2026-07-26 17:07:32 +0900

    readmeの調整

--- functions.php
+++ functions.php
@@ -86,7 +86,7 @@ function designo_theme_scripts() {
 	// フォント
 	wp_enqueue_style( 'designo-theme-fonts', 'https://fonts.googleapis.com/css2?family=Jost:wght@400;500;700&display=swap', false);
 	// js
-	wp_enqueue_script( 'designo-theme-js', get_template_directory_uri() . '/assets/js/script.js', array(), null, true);
+	wp_enqueue_script( 'designo-theme-js', get_template_directory_uri() . '/assets/js/script.js', array(), filemtime(get_template_directory() . '/assets/js/script.js'), true);
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
