b9aa5e6eb3904305cab2288e41bf1e16ee59bd4c
yama <yamagaku5504@gmail.com>
2026-07-28 00:31:11 +0900

    ローディングアニメーション

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1 viewport-fit=cover">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
